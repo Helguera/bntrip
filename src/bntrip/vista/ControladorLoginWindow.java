1a88672aa3e20200d2cf0e5b929869248993af69
javier <javier@javier-X550JK>
2017-04-14 15:48:50 +0200

    Comienzo de la ventana de login

---
+++ src/bntrip/vista/ControladorLoginWindow.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bntrip.vista;
+
+/**
+ *
+ * @author javier
+ */
+public class ControladorLoginWindow {
+ LoginWindow miVista;
+ 
+ public ControladorLoginWindow(LoginWindow miVista){
+     this.miVista = miVista;
+     
+ }
+ 
+ 
+}
