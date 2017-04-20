b68b035353aa69fd0f332204d304507509a8841d
Velastroll <velastroll@gmail.com>
2017-04-20 12:15:04 +0200

    Try: JavaDoc HomeWindow

--- src/bntrip/vista/HomeWindow.java
+++ src/bntrip/vista/HomeWindow.java
@@ -331,6 +331,7 @@ public class HomeWindow extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    
     private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened		
          // TODO add your handling code here:		
      }//GEN-LAST:event_formWindowOpened
