7460e317921cd4e34103a9e685af8b68f11c8425
javier <javier@javier-X550JK>
2017-04-22 17:11:05 +0200

    Falta caso 6 y 7 (reserva y deseado)

--- src/bntrip/main/Main.java
+++ src/bntrip/main/Main.java
@@ -5,6 +5,9 @@
  */
 package bntrip.main;
 import bntrip.modelo.Modelo;
+import bntrip.util.Alojamiento;
+import bntrip.util.Fechas;
+import bntrip.util.Reserva;
 import bntrip.vista.StateMachine.HomeUIStateMachine;
 import bntrip.vista.StateMachine.LoginUIStateMachine;
 import bntrip.vista.LoginWindow.LoginWindow;
@@ -44,7 +47,7 @@ public class Main {
         /**
          * CASOS DISPONIBLES DE EJECUCION (n=argumento):
          *      *** Se recomienda ejecutar con argumento=0, para ver la aplicacion al completo***
-         *      *** Para poder iniciar sesion con correo primero tienes que haberte creado una cuenta (los datos se guardan en un txt)***
+         *      *** Para poder iniciar sesion con correo primero tienes que haberte creado una cuenta (los datos se guardan en un .txt)***
          *      
          *      n=0 -> Ejecucion "por defecto", todas las funcionalidades disponibles
          *      n=1 -> Ejecucion "Google", usuario registrado con Google
@@ -52,7 +55,8 @@ public class Main {
          *      n=3 -> Logeado con Google, destinos sólo en Madrid, de 1 huésped y Habitacion Privada
          *      n=4 -> Logeado con Facebook, destinos solo en Murcia, de menos de 200€ y Casa Entera
          *      n=5 -> Sin estar logeado, 3 Huespedes en Cadiz, para cualquier alojamiento.
-         *      n=6 -> Sin estar logeado, alo
+         *      n=6 -> Estando logeado con Google, reserva hecha en Barcelona
+         *      n=7 -> Estando logeado con Facebook, alojamiento deseado en Barcelona
          */
         
         
@@ -84,8 +88,23 @@ public class Main {
                 modelo.setHuespedes(3);
                 break;
             case "6":
+                //Aqui va el ejemplo de una reserva
+                
+                modelo.setLoged("Google");
+                
+                Alojamiento prueba = new Alojamiento("Prueba", "Barcelona", 200, "Casa Entera", 2);
+                modelo.selectAlojamiento(prueba);
+                //Fechas fechas = new Fechas("Sat Apr 22 16:57:55 CEST 2017", "Sat Apr 28 16:57:55 CEST 2017");
+                //Reserva reserva = new Reserva(prueba, fechas ,2);
+                modelo.setReserva(prueba);
                 modelo.setLoged("Google");
                 modelo.setCiudad("Barcelona");
+                break;
+            case "7":
+                //Aqui va el ejemplo de un deseado
+                modelo.setLoged("Facebook");
+                
+                
                 break;
             default:
                 JOptionPane.showMessageDialog(null, "Ejecuta el programa con un argumento entre 0 y 6 (Ambos inclusive)", "Error", 0);
@@ -100,6 +119,7 @@ public class Main {
         homeStateMachine = new HomeUIStateMachine(modelo);
         
     }
+    
     
     public static LoginUIStateMachine getStateMachineLogin() {
         return loginStateMachine;
